8c1ca1f2b15ecd1adb41ff4ccf3dcc9c7a5853da
Muhammad Rizky Aulia Gobel <muhrizkyauliagobel@gmail.com>
2024-06-18 21:57:43 +0700

    Added new routes

--- HealthyEat - Cloud Computing/src/routes/tierList.route.js
+++ HealthyEat - Cloud Computing/src/routes/tierList.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
-const { getFruitById, getFruit, addFruit} = require(path.join(__dirname, '../controller/userController.js'));
+const { getFruitById, getFruitIncrease, getFruitDecrease, addFruit, getFruit} = require(path.join(__dirname, '../controller/userController.js'));
 const router = express.Router();
 
+router.post("/fruit/increase", getFruitIncrease);
+router.post("/fruit/decrease", getFruitDecrease);
 router.post("/fruit", getFruit);
-router.post("/fruit/:id", getFruitById);
+router.post("/fruit/:fruit_id", getFruitById);
 router.get("/add", addFruit);
 
 module.exports = router;
